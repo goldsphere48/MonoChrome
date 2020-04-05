ff455fe50273c45c5ab5fda30a5a8289bbbafe4e
goldsphere <goldsphere48@gmail.com>
2020-04-05 16:42:33 +1000

    Add Transform tests.

--- MonoChrome.Tests/TransformTest.cs
+++ MonoChrome.Tests/TransformTest.cs
@@ -31,7 +31,7 @@ namespace MonoChrome.Tests
         }
 
         [TestMethod]
-        public void Transform_Group_Change_AbsolutePosition_Children_Change_AbsolutePosition_Realetive_Parent()
+        public void Transform_Group_Change_AbsolutePosition_And_Children_Change_AbsolutePosition_Relative_Parent()
         {
             //Arrange
             Transform t1 = new Transform();
@@ -46,7 +46,7 @@ namespace MonoChrome.Tests
         }
 
         [TestMethod]
-        public void Transform_Group_Change_AbsolutePosition_Children_LocalPosition_Doesnt_Change()
+        public void Transform_Group_Change_AbsolutePosition_And_Children_LocalPosition_Doesnt_Change()
         {
             //Arrange
             Transform t1 = new Transform();
@@ -58,6 +58,50 @@ namespace MonoChrome.Tests
             t1.Position = new Vector2(1, 1);
             //Expected
             Assert.AreEqual(t2Local, t2.LocalPosition);
+        }
+
+        [TestMethod]
+        public void Change_Local_Position_In_Group_To_1_1_And_AbsolutePosition_Equals_1_1()
+        {
+            //Arrange
+            Transform t1 = new Transform();
+            Transform t2 = new Transform();
+            t2.Position = new Vector2(2, 2);
+            t2.Parent = t1;
+            var t2Local = t2.LocalPosition;
+            //Actual
+            t1.Position = new Vector2(1, 1);
+            //Expected
+            Assert.AreEqual(t2Local, t2.LocalPosition);
+        }
+
+        [TestMethod]
+        public void Add_1_Children_AndChildrenCount_Equals_1()
+        {
+            //Arrange
+            Transform t1 = new Transform();
+            Transform t2 = new Transform();
+            t2.Parent = t1;
+            //Actual
+            var actual = t1.Childrens.Count;
+            var expected = 1;
+            //Expected
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_1_Children_And_Children_Count_Equals_0()
+        {
+            //Arrange
+            Transform t1 = new Transform();
+            Transform t2 = new Transform();
+            t2.Parent = t1;
+            t2.Parent = null;
+            //Actual
+            var actual = t1.Childrens.Count;
+            var expected = 0;
+            //Expected
+            Assert.AreEqual(expected, actual);
         }
     }
 }
